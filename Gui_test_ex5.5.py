3be6a79f958c0ad8f30c6a17d56d2c3d7fefafb6
messyblanket <71762197+messyblanket@users.noreply.github.com>
2021-04-13 01:53:30 +0200

    Do not use overlay button. CSV rods added

--- Gui_test_ex5.5.py
+++ Gui_test_ex5.5.py
@@ -11,8 +11,11 @@ import os
 import sys
 import glob
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt
-from PyQt5.QtGui import QPalette, QImage
+from PyQt5.QtGui import QPalette, QImage, QPainter, QPixmap, QPen
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import pandas as pd
 
 
 class Ui_MainWindow(object):
@@ -24,6 +27,11 @@ class Ui_MainWindow(object):
         # self.dirReverser = None
         self.fileList = []
         self.currentfileindex = 0
+        # CSV FILE STUFF
+        color = 'blue'
+        col_list = ["particle", "frame", "x1_gp3", "x2_gp3", "y1_gp3", "y2_gp3"]
+        df_col = pd.read_csv('mark_rods/in_csv/rods_df_{:s}.csv'.format(color), usecols=col_list)
+        self.df_part = df_col
 
     # Main Window
     def setup_ui(self, MainWindow):
@@ -101,7 +109,7 @@ class Ui_MainWindow(object):
         # Signal to activate actions
         self.pushprevious.clicked.connect(self.show_prev)
         self.pushnext.clicked.connect(self.show_next)
-        self.overlay.clicked.connect(self.show_next)
+        self.overlay.clicked.connect(self.show_overlay)
         self.actionzoom_in.triggered.connect(self.zoomIn)
         self.actionzoom_out.triggered.connect(self.zoomOut)
         self.actionopen.triggered.connect(self.file_open)
@@ -145,6 +153,11 @@ class Ui_MainWindow(object):
         fileName, _ = QFileDialog.getOpenFileName(None, 'QFileDialog.getOpenFileName()', '',
                                                   'Images (*.png *.jpeg *.jpg)', options=options)
         file_name = os.path.split(fileName)[-1]
+
+        # CSV STUFF
+        df_part2 = self.df_part[self.df_part["frame"] == int(file_name[1:4])].reset_index()
+        # df_col.sort_values(by=['particle'], inplace=True)
+
         file_name = os.path.splitext(file_name)[0]          # File name without extension
         print('File name:', file_name)
 
@@ -155,6 +168,7 @@ class Ui_MainWindow(object):
             if image.isNull():
                 QMessageBox.information(self, "Image Viewer", "Cannot load %s." % fileName)
                 return
+
             # Directory
             dirpath = os.path.dirname(fileName)
             print('Dir name:', dirpath)
@@ -173,13 +187,60 @@ class Ui_MainWindow(object):
             # Sort according to name / ascending order
             self.fileList.sort()
             print('Num of items in list:', len(self.fileList))
+
             # Set read image into Label with Pixmap
-            self.Photo.setPixmap(QtGui.QPixmap.fromImage(image))
+            image2 = self.show_pixmap(image, df_part2)
+            self.Photo.setPixmap(QtGui.QPixmap.fromImage(image2))
             self.Photo.setScaledContents(True)
             self.scaleFactor = 1.0
             self.scrollArea.setVisible(True)
             self.fitToWindowAct.setEnabled(True)
             self.updateActions()
+            print('Open_file {}:'.format(self.currentfileindex), file_name)
+
+    def show_pixmap(self, image, df_part2):
+        pixmap = QPixmap(image)
+        painter = QPainter(pixmap)
+        pen = QPen(Qt.cyan, 3)
+        painter.setPen(pen)
+        # insert for loop
+        for ind_rod, value in enumerate(df_part2['particle']):
+            x1 = df_part2['x1_gp3'][ind_rod] * 10.0
+            x2 = df_part2['x2_gp3'][ind_rod] * 10.0
+            y1 = df_part2['y1_gp3'][ind_rod] * 10.0
+            y2 = df_part2['y2_gp3'][ind_rod] * 10.0
+            painter.drawLine(int(x1), int(y1), int(x2), int(y2))
+            painter.drawText(int(x1), int(y1), 20, 20, Qt.TextSingleLine, str(value))
+        painter.end()
+        image2 = QtGui.QPixmap.toImage(pixmap)
+        # to do, if seen is 0, then dotted
+        return image2
+
+
+    def show_overlay(self):
+        filename = (self.fileList[self.currentfileindex])  # Chooses next image with specified extension
+        file_name = os.path.split(filename)[-1]
+        image_name = file_name
+        pixmap = QPixmap(image_name)
+        painter = QPainter(pixmap)
+        pen = QPen(Qt.cyan, 3)
+        painter.setPen(pen)
+        col_list = ["particle", "frame", "x1_gp3", "x2_gp3", "y1_gp3", "y2_gp3"]
+        df_col = pd.read_csv('mark_rods/in_csv/rods_df_{:s}.csv'.format(self.color), usecols=col_list)
+        df_col = df_col[df_col["frame"] == int(image_name[1:4])].reset_index()  # insert frame name
+        #df_col.sort_values(by=['particle'], inplace=True)
+        # insert for loop
+        for ind_rod, value in enumerate(df_col['particle']):
+            x1 = df_col['x1_gp3'][ind_rod] * 10.0
+            x2 = df_col['x2_gp3'][ind_rod] * 10.0
+            y1 = df_col['y1_gp3'][ind_rod] * 10.0
+            y2 = df_col['y2_gp3'][ind_rod] * 10.0
+            painter.drawLine(int(x1), int(y1), int(x2), int(y2))
+            #painter.drawText(int(x1), int(y1), 20, 20, Qt.TextSingleLine, str(value))
+        painter.end()
+
+        # to do, if seen is 0, then dotted
+        self.Photo.setPixmap(pixmap)
 
     def show_next(self):
         if self.fileList:
@@ -187,9 +248,11 @@ class Ui_MainWindow(object):
                 self.currentfileindex += 1  # Increments file index
                 filename = (self.fileList[self.currentfileindex])  # Chooses next image with specified extension
                 file_name = os.path.split(filename)[-1]
+                # CSV STUFF
+                df_part2 = self.df_part[self.df_part["frame"] == int(file_name[1:4])].reset_index()
                 file_name = os.path.splitext(file_name)[0]
                 # Create Pixmap operator to display image
-                image_next = QtGui.QPixmap(filename).scaled(self.Photo.size(), QtCore.Qt.KeepAspectRatio)
+                image_next = QImage(filename)
                 if image_next.isNull():
                     # the file is not a valid image, remove it from the list
                     # and try to load the next one
@@ -197,7 +260,14 @@ class Ui_MainWindow(object):
                     self.show_next()
                 else:
                     # Set the image into Label with Pixmap
-                    self.Photo.setPixmap(image_next)
+                    image2 = self.show_pixmap(image_next, df_part2)
+                    #self.Photo.setPixmap(image2)
+                    self.Photo.setPixmap(QtGui.QPixmap.fromImage(image2))
+                    self.Photo.setScaledContents(True)
+                    self.scaleFactor = 1.0
+                    self.scrollArea.setVisible(True)
+                    self.fitToWindowAct.setEnabled(True)
+                    self.updateActions()
                     print('Next_file {}:'.format(self.currentfileindex), file_name)
             except:
                 # the iterator has finished, restart it
@@ -213,9 +283,11 @@ class Ui_MainWindow(object):
                 self.currentfileindex -= 1  # Decrements file index
                 filename = (self.fileList[self.currentfileindex])    # Chooses previous image with specified extension
                 file_name = os.path.split(filename)[-1]
+                # CSV STUFF
+                df_part2 = self.df_part[self.df_part["frame"] == int(file_name[1:4])].reset_index()
                 file_name = os.path.splitext(file_name)[0]
                 # Create Pixmap operator to display image
-                image_prev = QtGui.QPixmap(filename).scaled(self.Photo.size(), QtCore.Qt.KeepAspectRatio)
+                image_prev = QImage(filename)
                 if image_prev.isNull():
                     # the file is not a valid image, remove it from the list
                     # and try to load the next one
@@ -223,7 +295,13 @@ class Ui_MainWindow(object):
                     self.show_prev()
                 else:
                     # Set the image into Label with Pixmap
-                    self.Photo.setPixmap(image_prev)
+                    image2 = self.show_pixmap(image_prev,df_part2)
+                    self.Photo.setPixmap(QtGui.QPixmap.fromImage(image2))
+                    self.Photo.setScaledContents(True)
+                    self.scaleFactor = 1.0
+                    self.scrollArea.setVisible(True)
+                    self.fitToWindowAct.setEnabled(True)
+                    self.updateActions()
                     print('Prev_file {}:'.format(self.currentfileindex), file_name)
             except:
                 # the iterator has finished, restart it
