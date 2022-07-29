268ce25ac94692434bc3437d4a43a892f5c529b7
Adrian Niemann <adrian.niemann@gmx.de>
2022-07-29 09:20:12 +0200

    Factoring out data loading

---
+++ 3Dreconstruction/data_loading.py
@@ -0,0 +1,137 @@
+import json
+import numpy as np
+import cv2
+
+
+def extract_stereo_params(calibration_params: dict) -> dict:
+    """
+    OpenCV:             Matlab:
+    F                   FundamentalMatrix
+    E                   EssentialMatrix
+    T                   TranslationOfCamera2
+    R                   inv(RotationOfCamera2)
+
+    Matlab docs: (stereoParameters-rotationOfCamera2)
+    Rotation of camera 2 relative to camera 1, specified as a 3-by-3 matrix.
+    The rotationOfCamera2 and the translationOfCamera2 represent the relative
+    rotation and translation between camera 1 and camera 2, respectively.
+    They convert camera 2 coordinates back to camera 1 coordinates using:
+
+        orientation1 = rotationOfCamera2 * orientation2
+        location1 = translationOfCamera2 * orientation2 + location2
+
+    where, orientation1 and location1 represent the absolute pose of camera 1,
+    and orientation2 and location2 represent the absolute pose of camera 2.
+
+    OpenCV docs:
+    a) `stereoCalibrate()`:
+    R	    Output rotation matrix. Together with the translation vector T, this
+            matrix brings points given in the first camera's coordinate system
+            to points in the second camera's coordinate system. In more
+            technical terms, the tuple of R and T performs a change of basis
+            from the first camera's coordinate system to the second camera's
+            coordinate system. Due to its duality, this tuple is equivalent to
+            the position of the first camera with respect to the second camera
+            coordinate system.
+    T	    Output translation vector, see description above.
+    b) `stereoRectify()`:
+    R	    Rotation matrix from the coordinate system of the first camera to
+            the second camera, see stereoCalibrate.
+    T	    Translation vector from the coordinate system of the first camera
+            to the second camera, see stereoCalibrate.
+
+    If one computes the poses of an object relative to the first camera and to
+    the second camera, (R1, T1) and (R2, T2), respectively, for a stereo
+    camera where the relative position and orientation between the two cameras
+    are fixed, then those poses definitely relate to each other.
+    This means, if the relative position and orientation (R, T) of the two
+    cameras is known, it is possible to compute (R2, T2) when (R1, T1) is given.
+    This is what the described function does. It computes (R, T) such that:
+        R2=R*R1
+        T2=R*T1+T.
+
+    Therefore, one can compute the coordinate representation of a 3D point for
+    the second camera's coordinate system when given the point's coordinate
+    representation in the first camera's coordinate system:
+        ⎡X2⎤            ⎡X1⎤
+        ⎢Y2⎥ = ⎡R T⎤ *  ⎢Y1⎥
+        ⎢Z2⎥   ⎣0 1⎦    ⎢Z1⎥
+        ⎣ 1⎦            ⎣ 1⎦.
+
+    See https://de.mathworks.com/help/vision/ref/stereoparameters.html and
+    https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html for more info.
+    """
+    F = np.asarray(calibration_params["FundamentalMatrix"])
+    E = np.asarray(calibration_params["EssentialMatrix"])
+    R = np.linalg.inv(np.asarray(calibration_params["RotationOfCamera2"]))
+    T = np.asarray(calibration_params["TranslationOfCamera2"])
+    return {"F": F, "R": R, "T": T, "E": E}
+
+
+def extract_cam_params(mat_params: dict):
+    """
+        OpenCV:             Matlab:
+        [[fx, 0, cx],       [[fx, 0, 0],
+        [0, fy, cy],        [s, fy, 0],
+        [0, 0, 1]]          [cx, cy, 1]]
+
+        OpenCV:
+        (k1, k2, p1, p2[, k3[, k4, k5, k6[, s1, s2, s3, s4[, tau_x, tau_y]]]])
+        -> 4, 5, 8, 12 or 14 elements
+
+        - fx, fy -> focal lengths
+        - cx, cy -> Principal point
+        - k1, k2[, k3, k4, k5, k6] -> Radial (distortion) coefficients
+        - p1, p2 -> Tangential distortion coefficients
+        - s1, s2, s3, s4 -> prism distortion coefficients
+        - τx,τy -> angular parameters (for image sensor tilts)
+
+        see:
+        https://de.mathworks.com/help/vision/ref/cameraintrinsics.html
+        https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html
+        """
+    # mat_matrix = camera_parameters["IntrinsicMatrix"]
+    fx, fy = mat_params["FocalLength"]
+    cx, cy = mat_params["PrincipalPoint"]
+
+    # adjust for Matlab start index (1,1), see
+    # https://ch.mathworks.com/help/vision/ref/stereoparameterstoopencv.html)
+    cx = cx - 1
+    cy = cy - 1
+
+    cam_matrix = np.asarray(
+        [[fx, 0, cx],  # mtx/A/K in OpenCV
+         [0, fy, cy],
+         [0, 0, 1]])
+    ks = mat_params["RadialDistortion"]
+    ps = mat_params["TangentialDistortion"]
+    dist_coeffs = np.asarray([*ks[0:2], *ps, *ks[3:]])
+
+    return {
+        "matrix": cam_matrix,
+        "distortions": dist_coeffs,
+    }
+
+
+def load_calib_from_json(file_name: str) -> (dict, dict | None, dict | None):
+    with open(file_name, "r") as f:
+        all_calibs = json.load(f)
+    if "stereoParams" in all_calibs.keys():
+        cam1 = extract_cam_params(all_calibs["stereoParams"][
+                                         "CameraParameters1"])
+        cam2 = extract_cam_params(all_calibs["stereoParams"]["CameraParameters2"])
+        stereo_params = extract_stereo_params(all_calibs["stereoParams"])
+        stereo_params["img_size"] = all_calibs["stereoParams"][
+            "CameraParameters2"]["ImageSize"]
+        to_rectify = (
+            cam1["matrix"], cam1["distortions"], cam2["matrix"],
+            cam2["distortions"], stereo_params["img_size"], stereo_params["R"],
+            stereo_params["T"]
+        )
+        r1, r2, p1, p2, _, _, _ = cv2.stereoRectify(*to_rectify)
+        stereo_params.update({"R1": r1, "R2": r2, "P1": p1, "P2": p2})
+        return stereo_params, cam1, cam2
+
+    elif "transformations" in all_calibs.keys():
+        return all_calibs["transformations"]
+    return
--- 3Dreconstruction/matching.py
+++ 3Dreconstruction/matching.py
@@ -1,147 +1,18 @@
 import os.path
+import itertools
 
 import cv2
 import scipy.io as sio
 import numpy as np
 from scipy.optimize import linear_sum_assignment
-import json
-import itertools
+
+import data_loading as dl
 
 
-def extract_stereo_params(calibration_params: dict) -> dict:
-    """
-    OpenCV:             Matlab:
-    F                   FundamentalMatrix
-    E                   EssentialMatrix
-    T                   TranslationOfCamera2
-    R                   inv(RotationOfCamera2)
-
-    Matlab docs: (stereoParameters-rotationOfCamera2)
-    Rotation of camera 2 relative to camera 1, specified as a 3-by-3 matrix.
-    The rotationOfCamera2 and the translationOfCamera2 represent the relative
-    rotation and translation between camera 1 and camera 2, respectively.
-    They convert camera 2 coordinates back to camera 1 coordinates using:
-
-        orientation1 = rotationOfCamera2 * orientation2
-        location1 = translationOfCamera2 * orientation2 + location2
-
-    where, orientation1 and location1 represent the absolute pose of camera 1,
-    and orientation2 and location2 represent the absolute pose of camera 2.
-
-    OpenCV docs:
-    a) `stereoCalibrate()`:
-    R	    Output rotation matrix. Together with the translation vector T, this
-            matrix brings points given in the first camera's coordinate system
-            to points in the second camera's coordinate system. In more
-            technical terms, the tuple of R and T performs a change of basis
-            from the first camera's coordinate system to the second camera's
-            coordinate system. Due to its duality, this tuple is equivalent to
-            the position of the first camera with respect to the second camera
-            coordinate system.
-    T	    Output translation vector, see description above.
-    b) `stereoRectify()`:
-    R	    Rotation matrix from the coordinate system of the first camera to
-            the second camera, see stereoCalibrate.
-    T	    Translation vector from the coordinate system of the first camera
-            to the second camera, see stereoCalibrate.
-
-    If one computes the poses of an object relative to the first camera and to
-    the second camera, (R1, T1) and (R2, T2), respectively, for a stereo
-    camera where the relative position and orientation between the two cameras
-    are fixed, then those poses definitely relate to each other.
-    This means, if the relative position and orientation (R, T) of the two
-    cameras is known, it is possible to compute (R2, T2) when (R1, T1) is given.
-    This is what the described function does. It computes (R, T) such that:
-        R2=R*R1
-        T2=R*T1+T.
-
-    Therefore, one can compute the coordinate representation of a 3D point for
-    the second camera's coordinate system when given the point's coordinate
-    representation in the first camera's coordinate system:
-        ⎡X2⎤            ⎡X1⎤
-        ⎢Y2⎥ = ⎡R T⎤ *  ⎢Y1⎥
-        ⎢Z2⎥   ⎣0 1⎦    ⎢Z1⎥
-        ⎣ 1⎦            ⎣ 1⎦.
-
-    See https://de.mathworks.com/help/vision/ref/stereoparameters.html and
-    https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html for more info.
-    """
-    F = np.asarray(calibration_params["FundamentalMatrix"])
-    E = np.asarray(calibration_params["EssentialMatrix"])
-    R = np.linalg.inv(np.asarray(calibration_params["RotationOfCamera2"]))
-    T = np.asarray(calibration_params["TranslationOfCamera2"])
-    return {"F": F, "R": R, "T": T, "E": E}
-
-
-def extract_cam_params(mat_params: dict):
-    """
-        OpenCV:             Matlab:
-        [[fx, 0, cx],       [[fx, 0, 0],
-        [0, fy, cy],        [s, fy, 0],
-        [0, 0, 1]]          [cx, cy, 1]]
-
-        OpenCV:
-        (k1, k2, p1, p2[, k3[, k4, k5, k6[, s1, s2, s3, s4[, tau_x, tau_y]]]])
-        -> 4, 5, 8, 12 or 14 elements
-
-        - fx, fy -> focal lengths
-        - cx, cy -> Principal point
-        - k1, k2[, k3, k4, k5, k6] -> Radial (distortion) coefficients
-        - p1, p2 -> Tangential distortion coefficients
-        - s1, s2, s3, s4 -> prism distortion coefficients
-        - τx,τy -> angular parameters (for image sensor tilts)
-
-        see:
-        https://de.mathworks.com/help/vision/ref/cameraintrinsics.html
-        https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html
-        """
-    # mat_matrix = camera_parameters["IntrinsicMatrix"]
-    fx, fy = mat_params["FocalLength"]
-    cx, cy = mat_params["PrincipalPoint"]
-
-    # adjust for Matlab start index (1,1), see
-    # https://ch.mathworks.com/help/vision/ref/stereoparameterstoopencv.html)
-    cx = cx - 1
-    cy = cy - 1
-
-    cam_matrix = np.asarray(
-        [[fx, 0, cx],  # mtx/A/K in OpenCV
-         [0, fy, cy],
-         [0, 0, 1]])
-    ks = mat_params["RadialDistortion"]
-    ps = mat_params["TangentialDistortion"]
-    dist_coeffs = np.asarray([*ks[0:2], *ps, *ks[3:]])
-
-    return {
-        "matrix": cam_matrix,
-        "distortions": dist_coeffs,
-    }
-
-
-def load_calib_from_json(file_name: str) -> (dict, dict | None, dict | None):
-    with open(file_name, "r") as f:
-        all_calibs = json.load(f)
-    if "stereoParams" in all_calibs.keys():
-        cam1 = extract_cam_params(all_calibs["stereoParams"][
-                                         "CameraParameters1"])
-        cam2 = extract_cam_params(all_calibs["stereoParams"]["CameraParameters2"])
-        stereo_params = extract_stereo_params(all_calibs["stereoParams"])
-        stereo_params["img_size"] = all_calibs["stereoParams"][
-            "CameraParameters2"]["ImageSize"]
-        to_rectify = (
-            cam1["matrix"], cam1["distortions"], cam2["matrix"],
-            cam2["distortions"], stereo_params["img_size"], stereo_params["R"],
-            stereo_params["T"]
-        )
-        r1, r2, p1, p2, _, _, _ = cv2.stereoRectify(*to_rectify)
-        stereo_params.update({"R1": r1, "R2": r2, "P1": p1, "P2": p2})
-        return stereo_params, cam1, cam2
-
-    elif "transformations" in all_calibs.keys():
-        return all_calibs["transformations"]
-    return
-
-
+# TODO: Extract method for "triangulation" from match_rods()
+#  The new method shall take cam1rods, cam2rods, stereo_params.
+#  The new method shall compute undistorted points, triangulated points and
+#  reprojection errors.
 def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                frame_numbers, calibration_file=None,
                transformation_file=None,
@@ -158,8 +29,11 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
 
-    stereo_params, cam1, cam2 = load_calib_from_json(calibration_file)
-    transforms = load_calib_from_json(transformation_file)
+    stereo_params, cam1, cam2 = dl.load_calib_from_json(calibration_file)
+    transforms = dl.load_calib_from_json(transformation_file)
+
+    all_repr_errs = []
+    all_rod_lengths = []
 
     for color in colors:
         f_out = output_folder + f"data3d_{color}/"
@@ -249,6 +123,7 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                          np.sum(repr_errs[:, 1:3], axis=1)], axis=0),
                     (len(undist_cam1), len(undist_cam2))
                 )
+            all_repr_errs.append(costs)
 
             cam1_ind, cam2_ind = linear_sum_assignment(costs)
             assignment_cost = costs[cam1_ind, cam2_ind].sum()
@@ -264,7 +139,6 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                 k = cam1_ind[m]
                 j = cam2_ind[m]
                 # TODO: transformation to world coordinates of the 3D point
-                # TODO: insert len of rod, midpoint of rod
                 if point_choices[k, j]:
                     # use point matching of (p11,p21) and (p12,p22)
                     out[idx_out, 0:6] = p_triang[k, j, 0::3, :].flatten()
@@ -283,9 +157,11 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                     out[idx_out, 14:] = rods_cam2[j, -1::-1].flatten()
                 idx_out += 1
 
+            all_rod_lengths.append(out[:, 9])
+
             file_out = f"{f_out}{idx:05d}.txt"
             np.savetxt(file_out, out, fmt="%.18f", delimiter=" ")
-    return
+    return np.array(all_repr_errs), np.array(all_rod_lengths)
 
 
 def example_match_rods():
