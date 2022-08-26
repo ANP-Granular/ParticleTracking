29dd0e9550d25d5021f180d87967bfd56ac09363
Adrian Niemann <adrian.niemann@gmx.de>
2022-08-26 10:19:06 +0200

    collab - debugging

--- reconstruct_3D/matching.py
+++ reconstruct_3D/matching.py
@@ -5,9 +5,46 @@ import cv2
 import scipy.io as sio
 import numpy as np
 from scipy.optimize import linear_sum_assignment
+from scipy.spatial.transform import Rotation as R
+import matplotlib.pyplot as plt
 
 import reconstruct_3D.data_loading as dl
 from reconstruct_3D.result_visualizations import matching_results
+
+
+debug = True
+
+def project_to_world(points, transforms: dict):
+    rotx = np.asarray(transforms["M_rotate_x"])
+    roty = np.asarray(transforms["M_rotate_y"])
+    rotz = np.asarray(transforms["M_rotate_z"])
+    tw1 = np.asarray(transforms["M_trans"])
+    tw2 = np.asarray(transforms["M_trans2"])
+    old_shape = points.shape
+    points = np.reshape(points, (-1, 3))
+    points = np.append(points, np.ones((len(points),1)), axis=1)
+    for i in range(len(points)):
+        points[i,:] = tw2.dot(rotx.dot(rotz.dot(roty.dot(
+            tw1.dot(points[i,:])))))
+    points = points[:,0:3].T
+    return np.reshape(points, old_shape)
+
+
+def project_from_world(points, transforms: dict):
+    rotx = np.asarray(transforms["M_rotate_x"])
+    roty = np.asarray(transforms["M_rotate_y"])
+    rotz = np.asarray(transforms["M_rotate_z"])
+    tw1 = np.asarray(transforms["M_trans"])
+    tw2 = np.asarray(transforms["M_trans2"])
+    old_shape = points.shape
+    points = np.reshape(points, (-1, 3))
+    points = np.append(points, np.ones((len(points),1)), axis=1)
+    for i in range(len(points)):
+        points[i,:] = tw1.dot(roty.dot(rotz.dot(rotx.dot(
+            tw2.dot(points[i,:])))))
+    points = points[:,0:3].T
+    return np.reshape(points, old_shape)
+
 
 
 # TODO: Extract method for "triangulation" from match_rods()
@@ -35,12 +72,15 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
 
     all_repr_errs = []
     all_rod_lengths = []
-
+    if debug:
+        all_reprs = {}
     for color in colors:
         f_out = output_folder + f"data3d_{color}/"
         if not os.path.exists(f_out):
             os.mkdir(f_out)
 
+        if debug:
+            tmp_reprs = []
         for idx in frame_numbers:
             # Load data
             cam1_file = cam1_folder + cam1_convention.format(idx=idx,
@@ -96,12 +136,30 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                 pairs_all[:, 1, :].squeeze().transpose())
             p_triang = np.asarray([p[0:3]/p[3] for p in p_triang.transpose()])
 
-            rotx = np.asarray(transforms["M_rotate_x"])
-            roty = np.asarray(transforms["M_rotate_y"])
-            rotz = np.asarray(transforms["M_rotate_z"])
-            tw1 = np.asarray(transforms["M_trans"])
-            tw2 = np.asarray(transforms["M_trans2"])
-            rot_trafo = tw1 *rotx*roty*rotz
+            # world_rot = R.from_matrix([
+            #     transforms["M_trans"],
+            #     transforms["M_rotate_x"],
+            #     transforms["M_rotate_y"],
+            #     transforms["M_rotate_z"],
+            #     transforms["M_trans2"]
+            # ])
+
+            # print(p_triang.shape)
+            # p_triang = np.append(p_triang, np.ones((len(p_triang),1)), axis=1)
+            # rotx = np.asarray(transforms["M_rotate_x"])
+            # roty = np.asarray(transforms["M_rotate_y"])
+            # rotz = np.asarray(transforms["M_rotate_z"])
+            # tw1 = np.asarray(transforms["M_trans"])
+            # tw2 = np.asarray(transforms["M_trans2"])
+            # for i in range(len(p_triang)):
+            #     # p_triang[i,:] = tw2.dot(rotx.dot(rotz.dot(roty.dot(tw1.dot(p_triang[i,:])))))
+            #     p_triang[i,:] = tw1.dot(p_triang[i,:])
+            # p_triang = p_triang[:,0:3].T
+            # print(p_triang.shape)
+
+            # p_triang = project_to_world(p_triang, transforms)
+            # p_triang = project_from_world(p_triang, transforms)
+
 
 
             # Reprojection to the image plane for point matching
@@ -142,12 +200,18 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
 
             point_choices = point_choices.reshape((len(rods_cam1), len(rods_cam2)))
             p_triang = p_triang.reshape((len(rods_cam1), len(rods_cam2), 4, 3))
+
+            # TODO: transformation to world coordinates of the 3D point
+            # p_triang = project_to_world(p_triang, transforms)
             out = np.zeros((len(cam1_ind), 2*3+3+1+4+4))
+            if debug:
+                debug_out = np.zeros((len(cam1_ind), 8))
+                repr_cam1 = np.reshape(repr_cam1, (len(rods_cam1), len(rods_cam2), 4, 2))
+                repr_cam2 = np.reshape(repr_cam2, (len(rods_cam1), len(rods_cam2), 4, 2))
             idx_out = 0
             for m in range(len(cam1_ind)):
                 k = cam1_ind[m]
                 j = cam2_ind[m]
-                # TODO: transformation to world coordinates of the 3D point
                 if point_choices[k, j]:
                     # use point matching of (p11,p21) and (p12,p22)
                     out[idx_out, 0:6] = p_triang[k, j, 0::3, :].flatten()
@@ -156,6 +220,10 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                         np.diff(p_triang[k, j, 0::3, :], axis=0))
                     out[idx_out, 10:14] = rods_cam1[k, :].flatten()
                     out[idx_out, 14:] = rods_cam2[j, :].flatten()
+                    if debug:
+                        debug_out[idx_out, 0:4] = repr_cam1[k, j, 0::3, :].flatten()
+                        debug_out[idx_out, 4:] = repr_cam2[k, j, 0::3, :].flatten()
+
                 else:
                     # use point matching of (p11,p22) and (p12,p21)
                     out[idx_out, 0:6] = p_triang[k, j, 1:3, :].flatten()
@@ -164,13 +232,42 @@ def match_rods(cam1_folder, cam2_folder, output_folder, colors,
                         np.diff(p_triang[k, j, 1:3, :], axis=0))
                     out[idx_out, 10:14] = rods_cam1[k, -1::-1].flatten()
                     out[idx_out, 14:] = rods_cam2[j, -1::-1].flatten()
+                    if debug:
+                        debug_out[idx_out, 0:4] = repr_cam1[k, j, 1:3, :].flatten()
+                        debug_out[idx_out, 4:] = repr_cam2[k, j, 1:3, :].flatten()
                 idx_out += 1
-
+            if debug:
+                to_plot = cam1_convention.format(idx=idx, color=color)
+                plot_rods(file=to_plot, rods=debug_out, color=color,save=True)
+                tmp_reprs.append(debug_out) 
             all_rod_lengths.append(out[:, 9])
 
             file_out = f"{f_out}{idx:05d}.txt"
             np.savetxt(file_out, out, fmt="%.18f", delimiter=" ")
+        if debug:
+                all_reprs[color] = tmp_reprs
+    if debug:
+        return np.array(all_repr_errs), np.array(all_rod_lengths), all_reprs
     return np.array(all_repr_errs), np.array(all_rod_lengths)
+
+def plot_rods(file: str, rods, color:str, save=False):
+    base_folder = "/home/niemann/Documents/ParticleDetection/reconstruct_3D" \
+                  "/testfiles"
+    img_file = base_folder + "/" + os.path.splitext(file)[0].split(f"_{color}")[0] + ".jpg"
+    img = cv2.imread(img_file)
+    width, height = img.shape[1], img.shape[0]
+    fig = plt.figure(frameon=False)
+    dpi = fig.get_dpi()
+    fig.set_size_inches(
+        (width + 1e-2) / dpi,
+        (height + 1e-2) / dpi,
+    )
+    ax1 = fig.add_axes([0, 0, 1, 1])
+    ax1.imshow(img)
+    ax1.axis("off")
+    for r in rods:
+        ax1.plot(r[0:3:2], r[1:4:2], color=color)
+    plt.show()
 
 
 def example_match_rods():
@@ -180,7 +277,7 @@ def example_match_rods():
     transformation_file = "/home/niemann/Documents/TrackingScripts/software" \
                           "/calib_matlab_05_2017/transformations.json"
     colors = ["blue", "green", "red", "yellow", "brown"]
-    base_folder = "/home/niemann/Documents/ParticleDetection/3Dreconstruction" \
+    base_folder = "/home/niemann/Documents/ParticleDetection/reconstruct_3D" \
                   "/testfiles"
     cam1_folder = base_folder + "/gp1/"
     cam1_convention = "FT2015_shot1_gp1_{idx:05d}_{color:s}.mat"
@@ -191,20 +288,70 @@ def example_match_rods():
     start_frame = 732
     end_frame = 736
     frame_numbers = list(range(start_frame, end_frame+1))
+    if debug:
+        colors = ["blue",]
+        base_folder = "/home/niemann/Documents/ParticleDetection"\
+                      "/reconstruct_3D/debug_files"
+        cam1_folder = base_folder + "/gp3/"
+        cam1_convention = "{idx:05d}_{color:s}.mat"
+        cam2_folder = base_folder + "/gp4/"
+        cam2_convention = "{idx:05d}_{color:s}.mat"
+        out_folder = base_folder + "/data3D/"
+        start_frame = 100
+        end_frame = 904
+        frame_numbers = list(range(start_frame, end_frame+1))
 
-    errs, lens = match_rods(cam1_folder, cam2_folder, out_folder, colors,
-                            frame_numbers, calibration_file,
-                            transformation_file, cam1_convention,
-                            cam2_convention)
-    err_vis = np.array([])
-    len_vis = np.array([])
-    for err, l in zip(errs, lens):
-        err_vis = np.concatenate([err_vis, err.flatten()])
-        len_vis = np.concatenate([len_vis, l.flatten()])
-    matching_results(err_vis, len_vis)
+    if debug:
+        _, _, reprs = match_rods(cam1_folder, cam2_folder, out_folder, colors,
+                                frame_numbers, calibration_file,
+                                transformation_file, cam1_convention,
+                                cam2_convention)
+
+        
+    else:
+        errs, lens = match_rods(cam1_folder, cam2_folder, out_folder, colors,
+                                frame_numbers, calibration_file,
+                                transformation_file, cam1_convention,
+                                cam2_convention)
+        err_vis = np.array([])
+        len_vis = np.array([])
+        for err, l in zip(errs, lens):
+            err_vis = np.concatenate([err_vis, err.flatten()])
+            len_vis = np.concatenate([len_vis, l.flatten()])
+        matching_results(err_vis, len_vis)
+
+def debug_matching():
+    import scipy.io as sio
+    col_names = ['x1_r', 'y1_r', 'z1_r', 'x2_r', 'y2_r', 'z2_r', 
+                 'x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x', 'y', 'z', 'l',
+                 'x1_gp3', 'y1_gp3', 'x2_gp3', 'y2_gp3', 'x1_gp4', 'y1_gp4', 
+                 'x2_gp4', 'y2_gp4', 'particle', 'frame']
+    dbg_data_format = "./debug_files/data3d_blueT/{:05d}.txt"
+    rods_exp = 12
+    frames = list(range(100, 905))
+    dbg_data = dl.load_positions_from_txt(dbg_data_format, col_names, frames)
+    raw_3d = dbg_data[['x1_r', 'y1_r', 'z1_r', 'x2_r', 'y2_r', 'z2_r']].to_numpy()
+    rods_cam1 = dbg_data[['x1_gp3', 'y1_gp3', 'x2_gp3', 'y2_gp3']].to_numpy()
+    rods_cam2 = dbg_data[['x1_gp4', 'y1_gp4', 'x2_gp4', 'y2_gp4']].to_numpy()
+    rods_cam1 = rods_cam1.reshape((-1, rods_exp, 4))
+    rods_cam2 = rods_cam2.reshape((-1, rods_exp, 4))
+    dt = np.dtype(
+        [('Point1', np.float, (2,)), ('Point2', np.float, (2,))])
+    for r_c1, r_c2, fr in zip(rods_cam1, rods_cam2, frames):
+        arr = np.zeros((rods_exp,), dtype=dt)
+        arr[:]['Point1'] = r_c1[:, 0:2]
+        arr[:]['Point2'] = r_c1[:, 2:]
+        sio.savemat(f"./debug_files/gp3/{fr:05d}_blue.mat", 
+                    {'rod_data_links': arr})
+        arr2 = np.zeros((rods_exp,), dtype=dt)
+        arr2[:]['Point1'] = r_c2[:, 0:2]
+        arr2[:]['Point2'] = r_c2[1, 2:]
+        sio.savemat(f"./debug_files/gp4/{fr:05d}_blue.mat", 
+                    {'rod_data_links': arr2})
 
 
 if __name__ == "__main__":
-    # example_match_rods()
+    # debug_matching()
+    example_match_rods()
     # match_rods()
     pass
