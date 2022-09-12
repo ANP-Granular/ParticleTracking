52f101d9cf1732578964ab3c68478483116f6bc5
Adrian Niemann <adrian.niemann@gmx.de>
2022-09-12 11:31:46 +0200

    Minor plotting improvements
    The plotting does not block until all graphs are generated.
    Bin sizes are automatically set to useful widths.

--- reconstruct_3D/result_visualizations.py
+++ reconstruct_3D/result_visualizations.py
@@ -14,19 +14,21 @@ def matching_results(reprojetion_errors: np.ndarray, rod_lengths: np.ndarray):
     # reprojetion_errors *= rep_norm
 
     plt.figure()
-    hist = plt.hist(reprojetion_errors, alpha=.3)
+    plt.hist(reprojetion_errors, alpha=.5,
+             bins=np.arange(0, reprojetion_errors.max(), 0.5))
     plt.axvline(np.mean(reprojetion_errors), color="g")
     plt.axvline(np.median(reprojetion_errors), color="r")
+    plt.xlim(left=0)
     plt.legend(["mean", "median", "errs"])
     plt.xlabel("Reprojection error (in mm)")
     plt.ylabel("Number of rods")
     plt.title("Distribution of reprojection errors.")
-    plt.show()
 
     plt.figure()
-    plt.hist(rod_lengths, alpha=.2)
+    plt.hist(rod_lengths, alpha=.5, bins=np.arange(0, rod_lengths.max(), 0.25))
     plt.axvline(np.mean(rod_lengths), color="g")
     plt.axvline(np.median(rod_lengths), color="r")
+    plt.xlim(left=0)
     plt.legend(["mean", "median", "errs"])
     plt.xlabel("Rod length (in mm)")
     plt.ylabel("Number of rods")
