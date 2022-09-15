10c8730fc67ef440a2cf7d1a3731779353f87e03
Adrian Niemann <adrian.niemann@gmx.de>
2022-09-15 11:19:39 +0200

    Misc

---
+++ experiments/scripts/processing_files.py
@@ -0,0 +1,12 @@
+def combine_assembled_csv():
+    import os
+    import utils.data_conversions as dc
+    base_path = "../../datasets/assembled_200-906_2D"
+    colors = [
+        "blue", "brown", "green", "lilac", "orange", "purple", "red", "yellow",
+    ]
+    files = [os.path.join(base_path, f"rods_df_{c}.csv")for c in colors]
+    print(dc.csv_combine(files))
+
+if __name__ == "__main__":
+    combine_assembled_csv()
