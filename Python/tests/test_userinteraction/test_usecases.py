78745b66c4506f51ab4b3edd009c0fb56fe299b6
Adrian Niemann <adrian.niemann@gmx.de>
2022-10-23 00:16:26 +0200

    Usecase tests
    Tests using the basic user action functions for testing common tasks
    performed by users utilizing the GUI elements. The state of the program
    is tested for correctness before/after each of these atomic actions.
    Tests are grouped into tests of the atomic user actions and into more
    complex combinations of these actions that shall mimic typical work
    flows.
    In parametrized tests the whole function would break, if one test is
    not succeeding. This is mitigated with a try-finally block to ensure
    teardown code is run.

---
+++ Python/tests/test_userinteraction/test_usecases.py
@@ -0,0 +1,139 @@
+"""Tests for typical usecases and cases where problems occurred in the past."""
+import pathlib
+from typing import List
+from PyQt5 import QtWidgets, QtCore
+from pytestqt.qtbot import QtBot
+from pytest import MonkeyPatch
+import pytest
+from RodTracker.ui.mainwindow import RodTrackWindow
+import RodTracker.backend.logger as lg
+import gui_actions as ga
+import conftest
+
+
+def teardown_replacements(mp: MonkeyPatch):
+    """Replaces QMessageBox behaviour to be able to automatically close the
+    RodTracker, when it has unsaved changes."""
+    mp.setattr(QtWidgets.QMessageBox, "clickedButton",
+               lambda *args, **kwargs: None)
+    mp.setattr(QtWidgets.QMessageBox, "exec",
+               lambda *args, **kwargs: None)
+# =============================================================================
+# =============================================================================
+
+
+def test_typical(main_window: RodTrackWindow, qtbot: QtBot,
+                 monkeypatch: MonkeyPatch, tmp_path: pathlib.Path):
+    """Attempt a typical workflow."""
+    try:
+        scenario = [
+            ga.OpenData(conftest.csv_data),
+            ga.OpenImage(conftest.cam1_img1),
+            ga.SwitchCamera(),
+            ga.OpenImage(conftest.cam2_img1),
+            ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ALL_ONE_CAM),
+            ga.SwitchCamera(),
+            ga.SwitchRodNumber(7, 12, lg.NumberChangeActions.CURRENT),
+            ga.SwitchFrame(1),
+            ga.SwitchFrame(1),
+            ga.ChangeRodPosition(12),
+            ga.CreateRod(25),
+            ga.SwitchCamera(),
+            ga.LengthAdjustment(QtCore.Qt.Key_T),
+            ga.SwitchColor("blue"),
+            ga.DeleteRod(12),
+            ga.SaveChanges()
+        ]
+        for action in scenario:
+            main_window = action.run(main_window, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+# =============================================================================
+# =============================================================================
+
+
+@pytest.mark.xfail(reason="Not (fully) implemented.")
+def test_open_rods_wo_imgs():
+    raise NotImplementedError
+# =============================================================================
+# =============================================================================
+
+
+attempted_saving = [
+    pytest.param(
+        [
+            ga.SaveChanges(),
+        ],
+        id="ImmediateSaving",
+    ),
+    pytest.param(
+        [
+            ga.ChangeRodPosition(12),
+            ga.SaveChanges(),
+            ga.SaveChanges(),
+        ],
+        id="RepeatedSaving"
+    ),
+    pytest.param(
+        [
+            ga.ChangeRodPosition(12),
+            ga.Undo(),
+            ga.SaveChanges(),
+        ],
+        id="PostUndoSaving"
+    )
+]
+
+
+@pytest.mark.xfail(reason="Not (fully) implemented.")
+@pytest.mark.parametrize("scenario", attempted_saving)
+def test_save_wo_changes(both_cams: RodTrackWindow, qtbot: QtBot,
+                         monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                         scenario: List[ga.UserAction]):
+    try:
+        for action in scenario:
+            both_cams = action.run(both_cams, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+    raise NotImplementedError
+# =============================================================================
+# =============================================================================
+
+
+@pytest.mark.xfail(reason="Not (fully) implemented.")
+def test_open_rod_after_changes():
+    raise NotImplementedError
+# =============================================================================
+# =============================================================================
+
+
+def test_undo_after_save(both_cams: RodTrackWindow, qtbot: QtBot,
+                         monkeypatch: MonkeyPatch, tmp_path: pathlib.Path):
+    """Attempt to revert an action that has been saved already."""
+    scenario = [
+        ga.ChangeRodPosition(12),
+        ga.SaveChanges(),
+        ga.Undo(),
+        ga.SaveChanges(),
+    ]
+    try:
+        for action in scenario:
+            both_cams = action.run(both_cams, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+# =============================================================================
+# =============================================================================
+
+
+@pytest.mark.xfail(reason="Not (fully) implemented.")
+def test_save_on_unloaded_img():
+    """
+    Attempt saveing changes from 2nd view without having images loaded
+    there.
+    """
+    raise NotImplementedError
+# =============================================================================
+# =============================================================================
---
+++ Python/tests/test_userinteraction/test_useractions.py
@@ -0,0 +1,306 @@
+"""Tests to verify the basic functionality of user actions before further
+tests.
+"""
+import sys
+import pathlib
+from typing import List
+from PyQt5 import QtWidgets, QtCore
+from RodTracker.ui.mainwindow import RodTrackWindow
+from pytestqt.qtbot import QtBot
+from pytest import MonkeyPatch
+import pytest
+import RodTracker.backend.logger as lg
+import gui_actions as ga
+if sys.version_info < (3, 9):
+    # importlib.resources either doesn't exist or lacks the files()
+    # function, so use the PyPI version:
+    import importlib_resources
+else:
+    # importlib.resources has files(), so use that:
+    import importlib.resources as importlib_resources
+
+
+def teardown_replacements(mp: MonkeyPatch):
+    """Replaces QMessageBox behaviour to be able to automatically close the
+    RodTracker, when it has unsaved changes."""
+    mp.setattr(QtWidgets.QMessageBox, "clickedButton",
+               lambda *args, **kwargs: None)
+    mp.setattr(QtWidgets.QMessageBox, "exec",
+               lambda *args, **kwargs: None)
+
+
+data_opening = [
+    pytest.param(
+        [
+            ga.OpenData(importlib_resources.files(
+                "RodTracker.resources.example_data").joinpath("csv")),
+        ], id="open-positions"
+    ),
+    pytest.param(
+        [
+            ga.OpenImage(importlib_resources.files(
+                "RodTracker.resources.example_data.images.gp3").joinpath(
+                    "0500.jpg")),
+        ], id="open-image"
+    ),
+]
+
+
+@pytest.mark.parametrize("actions", data_opening)
+def test_data_opening(main_window: RodTrackWindow, qtbot: QtBot,
+                      monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                      actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            main_window = action.run(main_window, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+position_operations = [
+    pytest.param(
+        [
+            ga.CreateRod(25),
+        ], id="create rod"
+    ),
+    pytest.param(
+        [
+            ga.DeleteRod(12),
+        ], id="delete rod"
+    ),
+    pytest.param(
+        [
+            ga.ChangeRodPosition(12),
+        ], id="change position"
+    ),
+]
+
+
+@pytest.mark.parametrize("actions", position_operations)
+def test_position_operations(one_cam: RodTrackWindow, qtbot: QtBot,
+                             monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                             actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            one_cam = action.run(one_cam, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+number_changing = [
+    pytest.param(
+        [
+            ga.SwitchRodNumber(12, 7)
+        ], id="switch number-abort"
+    ),
+    pytest.param(
+        [
+            ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ALL)
+        ], id="switch number-all",
+    ),
+    pytest.param(
+        [
+            ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ALL_ONE_CAM)
+        ], id="switch number-one cam"
+    ),
+    pytest.param(
+        [
+            ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ONE_BOTH_CAMS)
+        ], id="switch number-one frame",
+    ),
+    pytest.param(
+        [
+            ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.CURRENT)
+        ], id="switch number-this frame/cam"
+    ),
+]
+
+
+@pytest.mark.parametrize("actions", number_changing)
+def test_number_changing(one_cam: RodTrackWindow, qtbot: QtBot,
+                         monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                         actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            one_cam = action.run(one_cam, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+length_adjustment = [
+    pytest.param(
+        [
+            ga.LengthAdjustment(QtCore.Qt.Key_A, 12),
+        ], id="Lengthen-single"
+    ),
+    pytest.param(
+        [
+            ga.LengthAdjustment(QtCore.Qt.Key_S, 12),
+        ], id="Shorten-single"
+    ),
+    pytest.param(
+        [
+            ga.LengthAdjustment(QtCore.Qt.Key_R, 12),
+        ], id="Lengthen-all"
+    ),
+    pytest.param(
+        [
+            ga.LengthAdjustment(QtCore.Qt.Key_T, 12),
+        ], id="Shorten-all"
+    ),
+]
+
+
+@pytest.mark.parametrize("actions", length_adjustment)
+def test_length_adjustment(both_cams: RodTrackWindow, qtbot: QtBot,
+                           monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                           actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            both_cams = action.run(both_cams, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+display_operations = [
+    pytest.param(
+        [
+            ga.SwitchColor("blue"),
+        ], id="switch color"
+    ),
+    pytest.param(
+        [
+            ga.SwitchFrame(2)
+        ], id="switch frame - positive, multi"
+    ),
+    pytest.param(
+        [
+            ga.SwitchFrame(-2)
+        ], id="switch frame - negative, multi"
+    ),
+    pytest.param(
+        [
+            ga.SwitchCamera()
+        ], id="switch camera"
+    ),
+]
+
+
+@pytest.mark.parametrize("actions", display_operations)
+def test_display_operations(both_cams: RodTrackWindow, qtbot: QtBot,
+                            monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                            actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            both_cams = action.run(both_cams, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+amendment_operations = [
+    pytest.param(
+        [
+            ga.DeleteRod(12),
+            ga.Undo(),
+        ], id="undo"
+    ),
+    pytest.param(
+        [
+            ga.DeleteRod(12),
+            ga.Undo(),
+            ga.Redo()
+        ], id="redo"
+    ),
+]
+
+
+@pytest.mark.parametrize("actions", amendment_operations)
+def test_amendment_operations(one_cam: RodTrackWindow, qtbot: QtBot,
+                              monkeypatch: MonkeyPatch, tmp_path: pathlib.Path,
+                              actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            one_cam = action.run(one_cam, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+operations = [
+    ga.CreateRod(25),
+    ga.DeleteRod(12),
+    ga.ChangeRodPosition(12),
+    ga.SwitchRodNumber(12, 7),
+    ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ALL),
+    ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ALL_ONE_CAM),
+    ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.ONE_BOTH_CAMS),
+    ga.SwitchRodNumber(12, 7, lg.NumberChangeActions.CURRENT),
+    ga.LengthAdjustment(QtCore.Qt.Key_A, 12),
+    ga.LengthAdjustment(QtCore.Qt.Key_S, 12),
+    ga.LengthAdjustment(QtCore.Qt.Key_R, 12),
+    ga.LengthAdjustment(QtCore.Qt.Key_T, 12),
+]
+undo_operations = []
+redo_operations = []
+for op in operations:
+    mark_condition = (
+        isinstance(op, (ga.LengthAdjustment,)) or
+        (isinstance(op, ga.SwitchRodNumber) and op.mode is None)
+    )
+    mark = pytest.mark.xfail(
+        condition=mark_condition,
+        reason=f"Incomplete {op.__class__.__name__}.undo() implementation",
+        strict=True
+    )
+    undo_operations.append(
+        pytest.param([op, ga.Undo()], id=f"undo-{op}", marks=mark)
+    )
+    redo_operations.append(
+        pytest.param([op, ga.Undo(), ga.Redo()], id=f"redo-{op}", marks=mark)
+    )
+
+
+@pytest.mark.parametrize("actions", [*undo_operations, *redo_operations])
+def test_amendmendable_operations(both_cams: RodTrackWindow, qtbot: QtBot,
+                                  monkeypatch: MonkeyPatch,
+                                  tmp_path: pathlib.Path,
+                                  actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            both_cams = action.run(both_cams, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
+
+
+save_operations = []
+for op in operations:
+    mark_condition = (
+        (isinstance(op, ga.SwitchRodNumber) and op.mode is None)
+    )
+    mark = pytest.mark.xfail(
+        condition=mark_condition,
+        reason=f"Saving of {op} is not intended.",
+        strict=True
+    )
+    save_operations.append(
+        pytest.param([op, ga.SaveChanges()], id=f"save-{op}", marks=mark)
+    )
+
+
+@pytest.mark.parametrize("actions", save_operations)
+def test_savable_operations(both_cams: RodTrackWindow, qtbot: QtBot,
+                            monkeypatch: MonkeyPatch,
+                            tmp_path: pathlib.Path,
+                            actions: List[ga.UserAction]):
+    try:
+        for action in actions:
+            both_cams = action.run(both_cams, qtbot, monkeypatch, tmp_path)
+    finally:
+        # teardown
+        teardown_replacements(monkeypatch)
