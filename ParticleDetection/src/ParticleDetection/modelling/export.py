899feb0c1d4ce24108d4532110bc1a50e87fe3a9
Adrian Niemann <adrian.niemann@gmx.de>
2023-07-03 13:35:26 +0200

    Add function to export predictions to json

--- ParticleDetection/src/ParticleDetection/modelling/export.py
+++ ParticleDetection/src/ParticleDetection/modelling/export.py
@@ -21,11 +21,15 @@ Functions to export a Detectron2 model as a pure pytorch model.
 **Date:**       31.10.2022
 
 """
+import json
 import logging
 from pathlib import Path
 from typing import Literal
-import torch
+
+import cv2
 import numpy as np
+from skimage.measure import approximate_polygon
+import torch
 from detectron2.config import CfgNode
 from detectron2.engine import DefaultPredictor
 # Don't remove, registers model parts in detectron2
@@ -86,3 +90,92 @@ def export_model(config_path: Path, weights_path: Path, sample_img: Path,
     save_path = Path(f"./model_{cfg.MODEL.DEVICE}.pt").resolve()
     torch.jit.save(traced_model, str(save_path))
     _logger.info(f"Exported model to '{str(save_path)}'")
+
+
+def annotation_to_json(prediction: dict, image: Path | str,
+                       output: Path = Path("./extracted_meta_data.json"),
+                       classes: dict = None):
+    """Saves detected object masks in the metadata format used for model
+    training.
+
+    Parameters
+    ----------
+    prediction : dict
+        Prediction output of a Detectron2 network with the actual results
+        present in ``prediction["instances"]`` as
+        ``detectron2.structures.Instances`` or ``dict``.
+    image : Path | str
+        Path to image the prediction was run on.
+    output : Path, optional
+        Path to the ``*.json`` file the annotation data should be saved in.
+        Already existing data for an image in this file will be overwritten.
+        By default ``Path("./extracted_meta_data.json")``.
+    classes: dict, optional
+        Dictionary of classes detectable by the model with\n
+        ``{key}``  ->  Index of class in the model\n
+        ``{value}`` ->  Name of the class\n
+        By default ``None``.
+    """
+    meta_data = {}
+    output = output.resolve()
+    if output.exists():
+        try:
+            with open(output, "r") as f:
+                meta_data = json.load(f)
+        except json.JSONDecodeError:
+            # overwrite the file
+            _logger.warning(f"Metadata file is not readable and will "
+                            f"be overwritten: {output}")
+    if isinstance(image, str):
+        image = Path(image)
+    image = image.resolve()
+    image_size = image.stat().st_size
+    image_id = image.name + str(image_size)
+
+    meta_data[image_id] = {
+        "filename": image.name,
+        "size": image_size,
+        "regions": [],
+    }
+
+    if "instances" in prediction.keys():
+        prediction = prediction["instances"].get_fields()
+    for k, v in prediction.items():
+        prediction[k] = v.to("cpu")
+
+    if classes is None:
+        classes = {cl: "not_defined" for cl in set(
+            prediction["pred_classes"].tolist())}
+
+    for i in range(len(prediction["pred_masks"])):
+        predicted_class = int(prediction["pred_classes"][i])
+        region = {
+            "shape_attributes": {
+                "name": "polygon",
+                "all_points_x": [],
+                "all_points_y": []
+            },
+            "region_attributes": {
+                "name": classes[predicted_class],
+                "type": str(predicted_class),
+            }
+        }
+        idxs = np.nonzero(prediction["pred_masks"][i])
+        # [outer_points, 2]
+        points = np.asarray((idxs[:, 1], idxs[:, 0])).swapaxes(0, 1)
+
+        hull = cv2.convexHull(points).squeeze()
+        if len(hull) > 20:
+            hull_prev = len(hull)
+            hull = approximate_polygon(hull, 1)
+            if hull_prev == len(hull):
+                print("Problem with simplification!")
+
+        region["shape_attributes"]["all_points_x"] = hull[:, 0].tolist()
+        region["shape_attributes"]["all_points_y"] = hull[:, 1].tolist()
+
+        meta_data[image_id]["regions"].append(region)
+
+    with open(output, "w") as f:
+        _logger.info(f"Saving metadata to {output}")
+        json.dump(meta_data, f, indent=2)
