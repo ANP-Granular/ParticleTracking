9035806321150c20323ae0318d425edadc46cdd2
Dmitry Puzyrev <dmitrypuzyrev@gmail.com>
2025-04-18 11:45:42 +0200

    a couple of changes, preparation for a new release tag

--- RodTracker/CHANGELOG.md
+++ RodTracker/CHANGELOG.md
@@ -1,7 +1,8 @@
 ## [v0.6.6]
 
 ### Added
-- a couple of warnings added which were missing in the last release
+- a couple of warnings (when data is loaded only from one camera) added which were missing in the last release
+- added a warning when no particles are detected in a given frame 
 - new flake8 version warnings (F824) suppressed (will be addressed in the new releases)
 
 ### Fixed
--- RodTracker/src/RodTracker/backend/detection.py
+++ RodTracker/src/RodTracker/backend/detection.py
@@ -22,6 +22,7 @@ Includes objects and methods used for detection of particles by RodTracker.
 """
 
 import logging
+import warnings
 from pathlib import Path
 from typing import Dict, List
 
@@ -274,6 +275,15 @@ class Detector(QtCore.QRunnable):
                     outputs, self.classes, expected_particles=self.expected
                 )
                 tmp_data = ds.add_points(points, data, self.cam_id, frame)
+            else:
+                warnings.warn(
+                    f"No particles detected on frame number {i}.",
+                    UserWarning,
+                )
+                _logger.warning(
+                    f"No particles detected on frame number {i}."
+                )
+                continue
             self.signals.progress.emit(1 / num_frames, tmp_data, self.cam_id)
         data.reset_index(drop=True, inplace=True)
         self.signals.finished.emit(self.cam_id)
